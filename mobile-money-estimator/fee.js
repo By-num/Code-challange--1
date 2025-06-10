4944f2b94046cff035dafcbe3a3ef076956e77de
By-num <bynumst3cy@gmail.com>
2025-06-10 15:49:32 +0300

    add HTML structure and configure live server settings

--- mobile-money-estimator/fee.js
+++ mobile-money-estimator/fee.js
@@ -1,15 +1,15 @@
 function estimateTransactionFee(amountToSend) {
-  let fee = amountToSend * 0.015;
+    let fee = amountToSend * 0.015;
 
-  if (fee < 10) {
-    fee = 10;
-  } else if (fee > 70) {
-    fee = 70;
-  }
+    if (fee < 10) {
+        fee = 10;
+    } else if (fee > 70) {
+        fee = 70;
+    }
 
-  const total = amountToSend + fee;
+    const total = amountToSend + fee;
 
-  console.log(`Sending KES ${amountToSend}:
+alert(`Sending KES ${amountToSend}:
 Calculated Transaction Fee: KES ${fee}
 Total amount to be debited: KES ${total}
 
